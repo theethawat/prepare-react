c51df595c0d4ae221ff7db3946fec86c3a0786e0
Theethawat <theethawat.s@outlook.co.th>
2019-01-20 12:30:56 +0700

    Make Alert When I click

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
     render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
+        return (
+            <button className="square" onClick={function(){alert('click')}}>
+              {this.props.value}
+            </button>
+          );
     }
   }
   
   class Board extends React.Component {
+      //เป็นการสร้างฟังก์ชันฟังก์ชั่นหนึ่ง
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i}/>;
     }
   
     render() {
